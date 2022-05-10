ac10aced154c26c9cc5a646d61466a90125a33a1
zshotan <zshotan@bloomberg.net>
2022-05-10 00:39:55 -0400

    Auto commit @ 2022-05-10T04:39:55Z

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 import logging
 
 REPO_PATH = os.path.dirname(os.path.abspath(__file__))
+VERSION_PATH = os.path.abspath(os.environ.get("VERSION_PATH", os.path.join(REPO_PATH, ".version")))
 GITHUB_URL = "https://github.com/LamaAni/yape.git"
 
 packages = find_packages()
@@ -18,13 +19,12 @@ with open(os.path.join(REPO_PATH, "requirements.txt"), "r") as requirements_file
     requirements_text = re.sub(r"[#].*", "", requirements_text)
     requirement_list = [r.strip() for r in requirements_text.split("\n") if len(r.strip()) > 0]
 
-version_file_path = os.path.join(os.path.dirname(__file__), ".version")
 version = None
-if os.path.isfile(version_file_path):
-    with open(version_file_path, "r") as file:
+if os.path.isfile(VERSION_PATH):
+    with open(VERSION_PATH, "r") as file:
         version = file.read()
 else:
-    logging.info("Version file not found @ " + version_file_path)
+    logging.info("Version file not found @ " + VERSION_PATH)
     logging.info("Using default version debug")
 
 version = version or os.environ.get("SETUP_VERSION", "debug")
