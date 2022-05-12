92079f67144f2364254ec2630cfd595648834d84
zshotan <zshotan@bloomberg.net>
2022-05-12 18:50:23 -0400

    Auto commit @ 2022-05-12T22:50:23Z

--- yapenv/cli/config.py
+++ yapenv/cli/config.py
@@ -7,51 +7,108 @@ from yapenv.cli.core import yapenv
 from yapenv.utils import clean_data_types
 
 
-@yapenv.command(
-    help="""Print the YAPENV computed configuration.
-DICT_PATHS (array) is a value to search, e.g. 'a.b[0].c'. If no paths provided
-will print the entire config.
-"""
-)
-@click.option("--resolve", help="Resolve requirement files", is_flag=True, default=None)
-@click.option("--allow-null", help="Return null values", is_flag=True, default=False)
-@FormatOptions.decorator(PrintFormat.yaml)
-@CommonOptions.decorator(path_as_option=True)
-@click.argument("dict_paths", nargs=-1)
-def config(
+@yapenv.group("config", help="Env configuration values")
+def config():
+    pass
+
+
+def get_config_values(
     dict_paths: List[str],
     resolve: bool = None,
     allow_null: bool = False,
     **kwargs,
 ):
     config = CommonOptions(kwargs).load(resolve_imports=resolve)
-    to_display = None
+    rslt = None
     was_found = False
     if len(dict_paths) == 0:
         # If no paths specified, display the entire config.
-        to_display = [config.to_dictionary()]
+        rslt = [config.to_dictionary()]
         was_found = True
     else:
         # Search for paths in the config
-        to_display = config.search(*dict_paths)
+        rslt = config.search(*dict_paths)
         # Clean the values from custom python types
-        to_display = [clean_data_types(v) for v in to_display]
-        was_found = len(to_display) > 0
+        rslt = [clean_data_types(v) for v in rslt]
+        was_found = len(rslt) > 0
 
     if not was_found:
         # Nothing was found. Throw error.
         raise ValueError(f"The dictionary path(s) were not found in the config, searched: {', '.join(dict_paths)}")
 
-    if not allow_null and any(v is None for v in to_display):
+    if not allow_null and any(v is None for v in rslt):
         raise ValueError("Found null values in path(s): " + ", ".join(dict_paths))
 
-    to_display = ["null" if v is None else v for v in to_display]
+    rslt = ["null" if v is None else v for v in rslt]
 
     if len(dict_paths) < 2:
         # If a single value requested, just display that value.
-        to_display = to_display[0]
+        rslt = rslt[0]
+
+    return rslt
+
+
+def print_config_values(
+    dict_paths: List[str],
+    resolve: bool = None,
+    allow_null: bool = False,
+    **kwargs,
+):
+    to_display = get_config_values(
+        dict_paths=dict_paths,
+        resolve=resolve,
+        allow_null=allow_null,
+        **kwargs,
+    )
 
     if not isinstance(to_display, list) and not isinstance(to_display, dict):
         print(str(to_display))  # Not a list or dict, don't format output.
     else:
         print(FormatOptions(kwargs).print(to_display))
+
+
+@config.command(
+    "view",
+    help="Print the yapenv computed configuration",
+)
+@click.option("--resolve", help="Resolve requirement files", is_flag=True, default=None)
+@click.option("--allow-null", help="Return null values", is_flag=True, default=False)
+@FormatOptions.decorator(PrintFormat.yaml)
+@CommonOptions.decorator()
+def view(
+    resolve: bool = None,
+    allow_null: bool = False,
+    **kwargs,
+):
+    print_config_values(
+        dict_paths=[],
+        resolve=resolve,
+        allow_null=allow_null,
+        **kwargs,
+    )
+
+
+@config.command(
+    "get",
+    help="""Print the YAPENV computed configuration.
+DICT_PATHS (array) is a value to search, e.g. 'a.b[0].c'. If no paths provided
+will print the entire config.
+""",
+)
+@click.option("--resolve", help="Resolve requirement files", is_flag=True, default=None)
+@click.option("--allow-null", help="Return null values", is_flag=True, default=False)
+@FormatOptions.decorator(PrintFormat.yaml)
+@CommonOptions.decorator(path_as_option=True)
+@click.argument("dict_paths", nargs=-1)
+def get(
+    dict_paths: List[str],
+    resolve: bool = None,
+    allow_null: bool = False,
+    **kwargs,
+):
+    print_config_values(
+        dict_paths=dict_paths,
+        resolve=resolve,
+        allow_null=allow_null,
+        **kwargs,
+    )
